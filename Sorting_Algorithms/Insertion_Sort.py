429c387893defef884461369ec7acc15f7e9eccf
Ketan Singh <KetanSingh_vsec@yahoo.com>
2018-06-07 23:34:10 +0530

    fixing for Python 3

---
+++ Sorting_Algorithms/Insertion_Sort.py
@@ -0,0 +1,24 @@
+#===============================================================================
+""" Insertion Sort - Implemented in Python """
+#===============================================================================
+
+"""shift optimized"""
+def insertion_sort(A):
+    for i in range(1, len(A)):
+        curNum = A[i]
+        k = 0
+        for j in range(i-1, -2, -1):
+            k = j
+            if A[j] > curNum:
+                A[j+1] = A[j]
+            else:
+                break
+        A[k+1] = curNum
+    
+        
+
+#driver Function
+A = [5,4,3,2,1]
+print(A)
+insertion_sort(A)
+print(A)
